747891dad18e1268b1bff52ef0dd91d7f14bcb8d
Anirutha <aniruthasivakumar03@gmail.com>
2023-06-28 14:17:06 +0530

    all changes done

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 app.use("/users",userRouter )
+app.post("/upload", (req, res) => {
+    // use modules such as express-fileupload, Multer, Busboy
+    
+    setTimeout(() => {
+        console.log('file uploaded')
+        return res.status(200).json({ result: true, msg: 'file uploaded' });
+    }, 3000);
+});
+
+app.delete("/upload", (req, res) => {
+    console.log(`File deleted`)
+    return res.status(200).json({ result: true, msg: 'file deleted' });
+});
 app.listen(PORT, ()=>console.log(`Server started in localhost:${PORT}`));
+
+
+
+
