43bd4fc832da8fd2fb3e066cb15173e8c3ab5017
Anirutha <aniruthasivakumar03@gmail.com>
2023-06-28 14:07:17 +0530

    final changes done

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 import {MongoClient} from "mongodb"
 import Obj from "mongodb"
 
-const MongoURL = "mongodb+srv://anirutha:Mahathi03@cluster0.fpaiuyz.mongodb.net/?retryWrites=true&w=majority"
+const MongoURL = "mongodb+srv://mahathi:mahathi03@cluster0.d5cci2i.mongodb.net/?retryWrites=true&w=majority"
 async function createConnection(){
    const client = new MongoClient(MongoURL);
     await client.connect()
