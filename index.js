6decaf2f450666ecd0b0726f29a1d0b634c1e1bb
Anirutha <aniruthasivakumar03@gmail.com>
2023-07-21 19:38:41 +0530

    all changes done

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import dotenv from "dotenv";
 import cors from 'cors';
-import { userRouter } from "./Routers/users.js";
-
 
 dotenv.config();
 const app = express();
@@ -11,6 +9,19 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 app.use("/users",userRouter )
+app.post("/upload", (req, res) => {
+    // use modules such as express-fileupload, Multer, Busboy
+    
+    setTimeout(() => {
+        console.log('file uploaded')
+        return res.status(200).json({ result: true, msg: 'file uploaded' });
+    }, 3000);
+});
+
+app.delete("/upload", (req, res) => {
+    console.log(`File deleted`)
+    return res.status(200).json({ result: true, msg: 'file deleted' });
+});
 
 app.listen(PORT, ()=>console.log(`Server started in localhost:${PORT}`));
 
